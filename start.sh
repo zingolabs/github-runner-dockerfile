8515140e3b2b43a4077fce6d261d5e798f3377da
nachog00 <chonageo@protonmail.com>
2024-10-17 19:22:10 -0300

    Removed token echo

--- start.sh
+++ start.sh
@@ -16,8 +16,8 @@ fi
 
 REPOSITORY=$REPO
 
-echo "REPO ${REPOSITORY}"
-echo "ACCESS_TOKEN ${ACCESS_TOKEN}"
+# echo "REPO ${REPOSITORY}"
+# echo "ACCESS_TOKEN ${ACCESS_TOKEN}"
 
 REG_TOKEN=$(curl -X POST -H "Authorization: token ${ACCESS_TOKEN}" -H "Accept: application/vnd.github+json" https://api.github.com/repos/${REPOSITORY}/actions/runners/registration-token | jq .token --raw-output)
 
